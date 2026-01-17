62b8bd558b9f782570a655be7d01ab1d857d8d5a
Aalpesh <aalpeshdayal@gmail.com>
2026-01-17 11:04:31 -0700

    Add CORS support and update installed apps
    Added 'corsheaders' to INSTALLED_APPS and MIDDLEWARE to enable CORS support. Configured CORS_ALLOWED_ORIGINS for local development. Also updated INSTALLED_APPS to include 'applications' instead of 'core'.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,16 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'applications',  
 ]
 
 MIDDLEWARE = [
+    'corsheaders',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +51,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
