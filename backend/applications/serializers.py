e93fbf34140a2149a9916cdcd3fe12008ea756cd
hassanfarooq00 <hassanfarooq00@gmail.com>
2026-01-20 01:01:39 -0700

    bug fix

--- backend/applications/serializers.py
+++ backend/applications/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 from .models import JobApplication, CommLog
 
+class CommLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommLog
+        fields = ["id", "job", "note", "created_at"]
+        read_only_fields = ["id", "job", "created_at"]
+
+
 # Aalpesh, changed 'def' to 'class' and added the inheritance
 class JobSerializer(serializers.ModelSerializer):
     logs = CommLogSerializer(many=True, read_only=True)
@@ -9,9 +16,3 @@ class JobSerializer(serializers.ModelSerializer):
         model = JobApplication
         fields = "__all__"
 
-
-class CommLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CommLog
-        fields = ["id", "job", "note", "created_at"]
-        read_only_fields = ["id", "job", "created_at"]
